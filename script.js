cf3644a2ecd55a19db17aa1b20909c22bd2860ce
SUPRIYA3037 <146860366+SUPRIYA3037@users.noreply.github.com>
2023-11-04 21:06:23 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,52 +1,107 @@
-let displayValue = '0';
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
 
-function updateDisplay() {
-    const display = document.getElementById('display');
-    display.value = displayValue;
-}
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
 
-function appendOperator(operator) {
-    const operators = ['+', '-', '*', '/'];
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+const particles = [];
 
-    if (operators.includes(displayValue.slice(-1))) {
-        // Remove the previous operator before adding the new one
-        displayValue = displayValue.slice(0, -1) + operator;
-    } else {
-        displayValue += operator;
+const img = new Image();
+img.src = './static/images/ekas.jpeg';
+
+const PARTICLE_DIAMETER = 7;
+img.addEventListener('load', function () {
+  // ctx.canvas.width  = window.innerWidth;
+  // ctx.canvas.height = window.innerHeight;
+  canvas.width = img.width;
+  canvas.height = img.height;
+  ctx.drawImage(img, 0, 0, img.width, img.height);
+  const imageData = ctx.getImageData(0, 0, img.width, img.height);
+  const numRows = Math.round(img.height / PARTICLE_DIAMETER);
+  const numCols = Math.round(img.width / PARTICLE_DIAMETER);
+
+  for(let row = 0; row < numRows; row++) {
+    for(let col = 0; col < numCols; col++) {
+      const pixelIndex = (row * PARTICLE_DIAMETER * img.width + col * PARTICLE_DIAMETER) * 4;
+
+      const red = imageData.data[pixelIndex];
+      const green = imageData.data[pixelIndex + 1];
+      const blue = imageData.data[pixelIndex + 2];
+      const alpha = imageData.data[pixelIndex + 3];
+
+      particles.push({
+        x: Math.floor(Math.random() * canvas.width),
+        y: Math.floor(Math.random() * canvas.height),
+        // x: col * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
+        // y: row * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
+        originX: col * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
+        originY: row * PARTICLE_DIAMETER + PARTICLE_DIAMETER / 2,
+        color: `rgba(${red}, ${green}, ${blue}, ${alpha/255})`
+      });
     }
+  }
+  drawParticles();
+}, false);
 
-    updateDisplay();
+function drawParticles() {
+  updateParticles();
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  particles.forEach(particle => {
+    ctx.beginPath();
+    ctx.arc(particle.x, particle.y, PARTICLE_DIAMETER / 2, 0, 2 * Math.PI);
+    ctx.fillStyle = particle.color;
+    ctx.fill();
+  });
+  requestAnimationFrame(drawParticles);
 }
 
-function appendNumber(number) {
-    displayValue += number;
-    updateDisplay();
-}
+let mouseX = Infinity;
+let mouseY = Infinity;
 
-function appendDecimal(decimal) {
-    if (!displayValue.includes(decimal)) {
-        displayValue += decimal;
-        updateDisplay();
+canvas.addEventListener('mousemove', function (e) {
+  mouseX = e.offsetX*1.5;
+  mouseY = e.offsetY*1.5;
+});
+
+canvas.addEventListener('mouseleave', function (e) {
+  mouseX = Infinity;
+  mouseY = Infinity;
+});
+
+function updateParticles() {
+  const REPEL_RADIUS = 50;
+  const REPEL_SPEED = 8;
+  const ATTRACTION_SPEED = 0.1;
+
+  particles.forEach(particle => {
+    const distanceFromMouseX = mouseX - particle.x;
+    const distanceFromMouseY = mouseY - particle.y;
+    const distanceFromMouse = Math.sqrt(distanceFromMouseX * distanceFromMouseX + distanceFromMouseY * distanceFromMouseY);
+
+    if(distanceFromMouse < REPEL_RADIUS) {
+      const angle = Math.atan2(distanceFromMouseY, distanceFromMouseX);
+      const force = ( REPEL_RADIUS - distanceFromMouse ) / REPEL_RADIUS;
+      const moveX = Math.cos(angle) * force * REPEL_SPEED;
+      const moveY = Math.sin(angle) * force * REPEL_SPEED;
+
+      particle.x -= moveX;
+      particle.y -= moveY;
+    }else if (
+      particle.x !== particle.originX ||
+      particle.y !== particle.originY
+    ) {
+      const distanceFromOriginX = particle.originX - particle.x;
+      const distanceFromOriginY = particle.originY - particle.y;
+      const distanceFromOrigin = Math.sqrt(distanceFromOriginX * distanceFromOriginX + distanceFromOriginY * distanceFromOriginY);
+      const angle = Math.atan2(particle.originY - particle.y, particle.originX - particle.x);
+      const moveX = Math.cos(angle) * distanceFromOrigin * ATTRACTION_SPEED;
+      const moveY = Math.sin(angle) * distanceFromOrigin * ATTRACTION_SPEED;
+
+      particle.x += moveX;
+      particle.y += moveY;
     }
-}
-
-function backspace() {
-    displayValue = displayValue.slice(0, -1);
-    updateDisplay();
-}
-
-function clearDisplay() {
-    displayValue = '';
-    updateDisplay();
-}
-
-function calculate() {
-    try {
-        const result = eval(displayValue);
-        displayValue = String(result);
-        updateDisplay();
-    } catch (error) {
-        displayValue = '';
-        updateDisplay();
-    }
+  });
 }
